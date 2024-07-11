74a230af017dee73fa7e2799991e90a1896714c4
DheerJoisher <dheerjoisher7@gmail.com>
2024-07-11 14:51:25 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import react from 'react' 
+
+function App() {
+
+  return (
+    <>
+      <h1 className='flex justify-center text-3xl'>Password Generator</h1>
+    </>
+  )
+}
+
+export default App
